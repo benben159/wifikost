009e6c8b9e7ded3a16b07e5b24a764e76c285a65
Benediktus Anindito <bennybroz105@gmail.com>
2021-06-22 14:10:09 +0700

    add command add-superadmin

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ import click
 
 from app import create_app, db, models, forms
 from tests import test_app
-
+from getpass import getpass
 app = create_app()
 
 
@@ -26,6 +26,20 @@ def drop_db():
     """Drop the current database."""
     db.drop_all()
 
+@app.cli.command()
+@click.argument('username')
+def add_superadmin(username):
+    user = models.User()
+    passwd1 = getpass("enter password:")
+    passwd2 = getpass("confirm password:")
+    if passwd1 != passwd2:
+        print("password do not match. exiting...")
+        exit(1)
+    user.username = username
+    user.password = passwd1
+    user.is_superadmin = True
+    user.save()
+    print("user {} created".format(username))
 
 if __name__ == '__main__':
     app.run()
