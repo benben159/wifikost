6714ce1f6fdee8368fe4b687b1a2b5c8adc0c068
Benediktus Anindito <bennybroz105@gmail.com>
2021-06-23 15:08:16 +0700

    add delete record function

--- app/utils.py
+++ app/utils.py
@@ -9,5 +9,10 @@ class ModelMixin(object):
         db.session.commit()
         return self
 
+    def delete(self):
+        ## FIXME CAREFULLY USE THIS METHOD! ##
+        db.session.delete(self)
+        db.session.commit()
+        return self
 
 # Add your own utility classes and functions here.
